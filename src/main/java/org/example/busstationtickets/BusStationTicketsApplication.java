b2955951bbdd06887b6ac5534e9677e8b30d65f7
Artem Vusyk <vusik.artiom06@gmail.com>
2025-06-15 12:21:06 +0300

    Initial commit

---
+++ src/main/java/org/example/busstationtickets/BusStationTicketsApplication.java
@@ -0,0 +1,13 @@
+package org.example.busstationtickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusStationTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusStationTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/busstationtickets/BusStationTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.busstationtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusStationTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
